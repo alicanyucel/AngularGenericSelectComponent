8978bf28567dd11ab43c18ee0d5de086e2b9677d
alicanyucel <yucelalican@hotmail.com>
2023-10-30 14:42:15 +0300

    AngularGenericSelectComponent

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { C1Component } from '../components/c1/c1.component';
+import { SelectComponent } from '../common/select/select.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    C1Component,
+    SelectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
